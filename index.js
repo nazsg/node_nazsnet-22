dcb150039e4454e88fbd5c40dca1dc73890fef88
Nazie Sharid <post@nazs.net>
2022-03-13 12:07:17 +0000

    added compression

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const menuRoute = require('./routes/menus')
 const productRoute = require('./routes/product')
 const expressHbs = require('express-handlebars')
 const cors = require('cors')
+const compression = require('compression')
 
 dotenv.config()
 
 // connect to DB
 mongoose.connect(
-  process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false},
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
   () => console.log('connected to db!')
 )
 
@@ -24,13 +26,14 @@ app.engine('hbs', expressHbs())
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(compression)
 app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json()) // based on body-parser, included since Express 4.16
 
 // Route Middlewares
@@ -40,7 +43,6 @@ app.use('/api/journals', journalRoute)
 app.use('/api/orders', orderRoute)
 app.use('/api/menus', menuRoute)
 app.use('/api/products', productRoute)
-
 
 app.use('/other', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'views', 'other.html'))
@@ -58,8 +60,8 @@ app.get('/', (req, res, next) => {
 })
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found again'})
+  res.status(404).render('404', { pageTitle: 'Page Not Found again' })
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
-app.listen(port=3020, () => console.log('Server up and running at port ' + port))
+app.listen((port = 3020), () => console.log('Server up and running at port ' + port))
